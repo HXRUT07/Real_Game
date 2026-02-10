97647225a5f006298ebcba504f256bac331d562c
Nipasiri <sayfanacha@gmail.com>
2026-02-10 16:21:02 +0700

    แก้เมืองให้มีตำแหน่งบนแผนที่

--- Real_Game/City.h
+++ Real_Game/City.h
@@ -6,14 +6,14 @@
 
 class City {
 private:
-    int gridR, gridC; // เมืองอยู่ที่ช่องไหน
+    int gridR, gridC;                // เมืองอยู่ช่องไหนใน grid
+    sf::Vector2f worldPos;           // ตำแหน่งจริงบนจอ
     std::vector<Building> buildings;
 
 public:
-    City(int r, int c);
+    City(int r, int c, sf::Vector2f pos);
 
     void addBuilding(BuildingType type);
     void draw(sf::RenderWindow& window);
-
     std::string getCityInfo();
 };
