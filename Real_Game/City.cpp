83205aee2aff53c8f1061a282cf0d911b3168b47
PPRAME6767 <nidmci9@gmail.com>
2026-02-28 20:09:13 +0700

    เปรม เพิ่มรูปปราสาท

--- Real_Game/City.cpp
+++ Real_Game/City.cpp
@@ -1,5 +1,6 @@
 ﻿#include "City.h"
 #include <sstream>
+#include <algorithm> // เปรมทำ - สำหรับ std::max
 
 //  ลบค่าในวงเล็บให้ตรงกับ City.h
 City::City(int r, int c, sf::Vector2f pos)
@@ -7,37 +8,38 @@ City::City(int r, int c, sf::Vector2f pos)
 {
     name = "New City";
 
-    baseIcon.setSize(sf::Vector2f(12.f, 12.f));
-    baseIcon.setFillColor(sf::Color::Yellow);
-    baseIcon.setOrigin(6.f, 6.f);
+    // เปรมทำ - โหลดรูปปราสาทแทนสี่เหลี่ยมสีเหลือง
+    if (castleTexture.loadFromFile("Blue Castle.png")) {
+        baseIcon.setTexture(castleTexture);
+        sf::Vector2u texSize = castleTexture.getSize();
+        float scale = 28.f / std::max(texSize.x, texSize.y);
+        baseIcon.setScale(scale, scale);
+        baseIcon.setOrigin(texSize.x / 2.f, texSize.y / 2.f);
+    }
     baseIcon.setPosition(center);
+    // เปรมทำ - จบ
 
     // ---ให้ทรัพยากรตั้งต้นนิดหน่อยตอนตั้งเมือง ---
     stockpile.gold = 100;
     stockpile.wood = 50;
     stockpile.food = 50;
 }
-
 void City::draw(sf::RenderWindow& window)
 {
     window.draw(baseIcon);
 }
-
 sf::FloatRect City::getBounds() const
 {
     return baseIcon.getGlobalBounds();
 }
-
 std::string City::getCityInfo() const
 {
     std::stringstream ss;
-
     ss << "City Name: " << name << "\n\n";
     // เปลี่ยนมาโชว์ของในคลังหลวง (stockpile) 
     ss << "Wood: " << stockpile.wood << "\n";
     ss << "Gold: " << stockpile.gold << "\n";
     ss << "Food: " << stockpile.food << "\n\n";
     ss << "Upgraded: " << (upgraded ? "Yes" : "No");
-
     return ss.str();
 }
--- Real_Game/City.h
+++ Real_Game/City.h
@@ -3,40 +3,35 @@
 #include <vector>
 #include <string>
 #include "ResourceManage.h"
-
 class City {
 private:
     int gridR, gridC;
     sf::Vector2f center;
-    sf::RectangleShape baseIcon;
+
+    // เปรมทำ - เปลี่ยนจาก RectangleShape เป็น Texture + Sprite
+    sf::Texture castleTexture;
+    sf::Sprite baseIcon;
+    // เปรมทำ - จบ
 
     std::string name;
     bool upgraded = false;
-
     // ---  คลังหลวงเก็บทรัพยากรของเมือง ---
     ResourceYield stockpile;
-
 public:
     // ลบ ResourceYield ในวงเล็บออก เพราะเราใช้คลังหลวงแทนแล้ว
     City(int r, int c, sf::Vector2f pos);
-
     int getR() const { return gridR; }
     int getC() const { return gridC; }
-
     void setName(const std::string& newName) { name = newName; }
     std::string getName() const { return name; }
-
     // ---ฟังก์ชันจัดการคลังหลวง ---
     int getGold() const { return stockpile.gold; }
     int getWood() const { return stockpile.wood; }
     int getFood() const { return stockpile.food; }
-
     void addGold(int amount) { stockpile.gold += amount; }
     void addWood(int amount) { stockpile.wood += amount; }
     void addFood(int amount) { stockpile.food += amount; }
-
     void draw(sf::RenderWindow& window);
-
     sf::FloatRect getBounds() const;
     std::string getCityInfo() const;
 };
